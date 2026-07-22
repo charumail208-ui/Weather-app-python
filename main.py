26a305182b57cbc96e2807619dc4c4ff16df5bf0
Charutha Rajeevan <charumail208@gmail.com>
2026-07-22 07:16:45 +0530

    day-9 displayed weather info using json parsing

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ city=input("Enter city name: ")
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 response=requests.get(url)
 if response.status_code==200:
-    print("Request successful!")
+    data=response.json()
+    temperature=data['main']['temp']
+    humidity=data['main']['humidity']
+    condition=data['weather'][0]['main']
+    description=data['weather'][0]['description']
+    wind_speed=data['wind']['speed']
+    print("--------------------------------------")
+    print(f"Weather in {city}:")
+    print("---------------------------------------")
+    print(f"City: {city}")
+    print(f"Temperature: {temperature}°C")
+    print(f"Humidity: {humidity}%")
+    print(f"Weather condition: {condition}")
+    print(f"Weather description: {description}")
+    print(f"Wind speed: {wind_speed} m/s")
 else:
-    print("Error occurred while fetching weather data.")
+    print("Error: Unable to fetch weather data. Please check the city name and try again.")
+if response.status_code == 404:
+    print("City not found. Please check the city name and try again.")
